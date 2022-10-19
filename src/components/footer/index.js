6292a322e0beb1512a5edf7c61ae79a7d4734ffb
Yamakasi25 <runilyasrun@mail.ru>
2022-10-19 16:19:33 +0300

    first commit husky

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import TaskFilter from '../task-filter'
+import TaskFilter from '../task-filter';
 import './footer.css'
 
 function Footer({ toDo, filter, onFilter, clearTask }) {
